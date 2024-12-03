eaf1a0e5733c3466a9b1c9b2274311532f67d8b7
chsa911 <ch.sa911@gmail.com>
2024-12-03 22:01:04 +0100

    JWT SECRET

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const router =  express.Router();
 
-const JWT_SECRET = process.env.JWT_SECRET_KEY
+const JWT_SECRET = 'aab';/*process.env.JWT_SECRET*/
 
 router.post("/admin", async (req, res) => {
     const {username, password} = req.body;
